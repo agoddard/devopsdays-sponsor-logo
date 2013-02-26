ba00428174f45aa8ec097288cc29ed875e89a1e8
Anthony Goddard <anthony@anthonygoddard.com>
2013-02-26 16:12:14 -0500

    5px padding

--- lib/sponsor.rb
+++ lib/sponsor.rb
@@ -6,12 +6,12 @@ include Magick
 def fetch_remote_logo(url)
   logo = Magick::ImageList.new
   logo.from_blob(open(url).read)
-  logo.resize_to_fit!(300, 300)
+  logo.resize_to_fit!(295, 295)
 end
 
 def fetch_local_logo(file)
   logo = Magick::ImageList.new(file)
-  logo.resize_to_fit!(300, 300)
+  logo.resize_to_fit!(295, 295)
 end
 
 def render_logo(logo)
@@ -22,6 +22,3 @@ def render_logo(logo)
   image.format = "png"
   return image  
 end
-
-
-render_logo(fetch_remote_logo('https://dl.dropbox.com/u/327815/pd_PD.png'))
